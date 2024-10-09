ee19532add68d75a2289880a0e1cd4b8fd746914
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-10-08 21:12:51 -0500

    asp.net addition

---
+++ fall2024-assignment3-sixao8/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using fall2024_assignment3_sixao8.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace fall2024_assignment3_sixao8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ fall2024-assignment3-sixao8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fall2024_assignment3_sixao8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
